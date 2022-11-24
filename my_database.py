9abb8d5e88adbc13048134267ee72242b4ed9d46
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-11-24 13:08:05 +0100

    added students end grades

--- my_database.py
+++ my_database.py
@@ -13,6 +13,9 @@ def create_connection(db_file):
     except Error as e:
         print(e)
     return conn
+    # finally:
+    #     if conn:
+    #         conn.close()
 
 def execute_sql(conn, sql): # sql - tekst, kod sql, który jest podany do zmiennej 
     #przekzanej do funkcji
@@ -30,9 +33,9 @@ def execute_sql(conn, sql): # sql - tekst, kod sql, który jest podany do zmienn
 def add_subject(conn, subject): 
     """
     Create a new project into the subjects table
-    :param conn:
+    :param conn: Connection object
     :param subject: subject name
-    :return: subject id
+    :return: print("added")
     """
     sql = '''INSERT INTO subjects(name_subject, teacher)
                 VALUES(?,?)'''
@@ -41,8 +44,25 @@ def add_subject(conn, subject):
     conn.commit()
     print("Added")
 
+def add_student(conn, student):
+    """
+    Create a new task into the students table
+    :param conn: Connection object
+    :param student: student data
+    :return: print("added")
+    """
+    sql = '''INSERT INTO students(subject_id, name, surname, year_of_study, subject_grade)
+        VALUES(?,?,?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, student)
+    conn.commit()
+    print("Added")
+
 if __name__ == '__main__':
-    
+    # wykonano
+    # Tworzę połączenie z bazą, chyba że nie istneije to tworzy z podaną nazwą
+    # create_connection(r"my_database.db")  
+
     create_subjects_sql = """
     -- subject table
     CREATE TABLE IF NOT EXISTS subjects (
@@ -61,13 +81,19 @@ if __name__ == '__main__':
         subject_grade FLOAT NOT NULL,
         FOREIGN KEY (subject_id) REFERENCES subjects (id));
     """
-    conn = create_connection("my_database.db")
-    if conn is not None:
-        execute_sql(conn,create_subjects_sql)
-        execute_sql(conn,create_students_sql)
-        conn.close()
+    # wykonano
+    # conn = create_connection("my_database.db")
+    # tworzę tabelę
+    # if conn is not None: 
+    #     execute_sql(conn,create_subjects_sql)
+    #     execute_sql(conn,create_students_sql)
+    #     conn.close()
 
-    conn = create_connection("my_database.db")
+    conn = create_connection("my_database.db") #połączenie z bazą
 
-    subject = ("Geaografia", "W. O'Conor")
-    add_subject(conn, subject) # wywołuję funkcję add_subject
+    # wykoanno
+    # subject = ("Geaografia", "W. O'Conor")
+    # add_subject(conn, subject) # wywołuję funkcję add_subject
+
+    student = (6, "Eric", "Goldman", 2, 5)
+    add_student(conn, student) # wywołuję funkcję add_student
